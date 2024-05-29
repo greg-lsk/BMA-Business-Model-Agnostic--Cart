fdb1d77034ae2e4a7d68bd376f8a164aac70e361
greg-lsk <greg.liaskas@outlook.com>
2024-05-29 17:29:23 +0300

    minor changes

--- Cart.cs
+++ Cart.cs
@@ -2,5 +2,14 @@
 
 public class Cart<TProduct>
 {
-    
+    private readonly CartItems<TProduct> _items = new();
+
+    //Add Product
+    //Delete Product
+
+    //Update Quantity of Product(increase AND reduce)
+
+    //Get Items
+
+    //Pricing
 }

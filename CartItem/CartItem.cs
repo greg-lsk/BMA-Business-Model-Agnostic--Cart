cb300a3da0d283747b52f8c8463306e8eb0d5d8a
greg-lsk <greg.liaskas@outlook.com>
2024-05-29 12:44:03 +0300

    Add CartItem<TProduct> struct for representing shopping cart items

---
+++ CartItem/CartItem.cs
@@ -0,0 +1,7 @@
+﻿namespace Cart;
+
+internal readonly struct CartItem<TProduct>(TProduct product, int quantity = 0)
+{
+    internal readonly int Quantity = quantity;
+    internal readonly TProduct Product = product;
+}

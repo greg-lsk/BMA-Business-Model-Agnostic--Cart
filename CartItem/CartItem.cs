0a6ffd6a43a8f03c480c9f752c56fe0a68d5f923
greg-lsk <greg.liaskas@outlook.com>
2024-05-29 13:36:31 +0300

    Impemented "GetHasCode()" and overridden "==" and "!=" operators

--- CartItem/CartItem.cs
+++ CartItem/CartItem.cs
@@ -4,4 +4,13 @@ internal readonly struct CartItem<TProduct>(TProduct product, int quantity = 0)
 {
     internal readonly int Quantity = quantity;
     internal readonly TProduct Product = product;
+
+
+    public static bool operator ==(CartItem<TProduct> item1, CartItem<TProduct> item2) 
+        => item1.Product.Equals(item2.Product);
+        
+    public static bool operator !=(CartItem<TProduct> item1, CartItem<TProduct> item2)
+        => !(item1 == item2);        
+
+    public override int GetHashCode() => Product.GetHashCode();
 }

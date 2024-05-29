05b6d19f86e9d8b2bc5ee21c53d6da2019d72d0a
greg-lsk <greg.liaskas@outlook.com>
2024-05-29 13:59:53 +0300

    made quantity parameter in 'Add' function not optional

--- CartItem/CartItems.cs
+++ CartItem/CartItems.cs
@@ -4,5 +4,5 @@ internal class CartItems<TProduct>(List<CartItem<TProduct>> items)
 {
     private readonly List<CartItem<TProduct>> _items = items;
 
-    internal void Add(TProduct product, int quantity = 0) => _items.Add(new CartItem<TProduct>(product, quantity));
+    internal void Add(TProduct product, int quantity) => _items.Add(new CartItem<TProduct>(product, quantity));
 }

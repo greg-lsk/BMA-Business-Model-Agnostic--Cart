aef6e0a697d47592f06dd0520b53d42ee4a62268
greg-lsk <greg.liaskas@outlook.com>
2024-06-01 20:28:28 +0300

    #draft:: make StockedCollection.Add() more modular

--- Cart.cs
+++ Cart.cs
@@ -7,12 +7,21 @@ public delegate int QuantityUpdateDelegate(int inCartQuantity);
 
 public class Cart<TProduct>(EqualityDelegate<TProduct> equalityDelegate)
 {
-    private readonly StockedCollection<TProduct> _items = new(equalityDelegate);
+    private readonly StockedCollection<TProduct> _items 
+    = new(equalityDelegate);
 
     public int CountDistinct => _items.CountDistinct;
     public int CountTotal => _items.CountTotal;
 
-    public void Add(TProduct product, int quantity = 1) => _items.Add(product, quantity);
+    public void Add(TProduct product, int quantity = 1) => 
+    _items.Add
+    (
+        product, 
+        quantity, 
+        () => _items.IterativeCheck( (p, q) => product.Equals(p) && q > 15 ), 
+        () => _items.UpdateQuantity(product, inCart => inCart + quantity) 
+    );
+ 
     public void Delete(TProduct product) => _items.Delete(product);
     public void UpdateQuantity(TProduct product, QuantityUpdateDelegate updateDelegate) 
         => _items.UpdateQuantity(product, updateDelegate);
--- CartItem/StockedCollection.cs
+++ CartItem/StockedCollection.cs
@@ -5,19 +5,20 @@ namespace Cart;
 internal class StockedCollection<TProduct>(EqualityDelegate<TProduct> equalityDelegate)
 {
     private readonly List<(TProduct Product, int Quantity)> _items = [];
+
     private readonly EqualityDelegate<TProduct> _equals = equalityDelegate;
 
     internal int CountDistinct => _items.Count;
     internal int CountTotal
     {
-        get 
+        get
         {
             int total = 0;
- 
-            for(int i=0; i < _items.Count; ++i)
-                total += _items[i].Quantity; 
-            
-            return total;  
+
+            for (int i = 0; i < _items.Count; ++i)
+                total += _items[i].Quantity;
+
+            return total;
         }
     }
 
@@ -27,57 +28,74 @@ internal class StockedCollection<TProduct>(EqualityDelegate<TProduct> equalityDe
         set => _items[index] = value;
     }
 
-    internal void Add(TProduct product, int quantity)
+    internal void Add(TProduct product,
+                      int quantity,
+                      Func<bool> updateCondition,
+                      Action updateAction)
     {
-        for(int i = 0; i < _items.Count; ++i)
-        {
-            var (Product, Quantity) = _items[i];
-
-            if(_equals(Product, product))
-            {
-                _items[i] = (Product, Quantity + quantity);
-                return;
-            }
-        }
-
-        _items.Add((product, quantity));
+        if (updateCondition()) updateAction();
+        else _items.Add((product, quantity));
     }
 
     internal void Delete(TProduct product)
     {
-        for(int i = 0; i < _items.Count; ++i)
+        for (int i = 0; i < _items.Count; ++i)
         {
-            if( _equals(_items[i].Product, product) ) 
-            { 
-                _items.RemoveAt(i); 
-                return; 
+            if (_equals(_items[i].Product, product))
+            {
+                _items.RemoveAt(i);
+                return;
             }
         }
     }
-   
+
     internal void UpdateQuantity(TProduct product, QuantityUpdateDelegate update)
     {
-        for(int i = 0; i < _items.Count; ++i)
+        for (int i = 0; i < _items.Count; ++i)
         {
             var (Product, Quantity) = _items[i];
-            if( _equals(Product, product) )
+            if (_equals(Product, product))
             {
                 _items[i] = (Product, update(Quantity));
                 return;
             }
-        } 
+        }
     }
 
     internal ReadOnlyCollection<(TProduct Product, int Quantity)> AsReadonly() => Array.AsReadOnly(_items.ToArray());
 
     internal int CountOf(TProduct product)
     {
-        for(int i = 0; i < _items.Count; ++i)
+        for (int i = 0; i < _items.Count; ++i)
         {
             var (Product, Quantity) = _items[i];
-            if( _equals(Product, product) ) return Quantity;
+            if (_equals(Product, product)) return Quantity;
         }
 
         return 0;
     }
+
+    internal bool Contains(TProduct product, EqualityDelegate<TProduct>? equalityDelegate = null)
+    {
+        equalityDelegate ??= _equals;
+
+        for (int i = 0; i < _items.Count; ++i)
+        {
+            if (equalityDelegate(_items[i].Product, product)) return true;
+        }
+
+        return false;
+    }
+
+    internal bool IterativeCheck(Func<TProduct, int, bool> check)
+    {
+        for (int i = 0; i < _items.Count; ++i)
+        {
+            var (Product, Quantity) = _items[i];
+            if (check(Product, Quantity)) /*do something here, on hit*/ return true;
+        }
+
+        /*Do something here on miss*/
+        return false;
+    } 
 }

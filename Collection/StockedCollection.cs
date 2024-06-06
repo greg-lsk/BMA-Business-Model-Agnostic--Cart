893834244fe462f8c08ccdd61f4503ab5e1a940f
greg-lsk <greg.liaskas@outlook.com>
2024-06-07 00:55:03 +0300

    file-created:: Iteration.cs

--- Collection/StockedCollection.cs
+++ Collection/StockedCollection.cs
@@ -85,18 +85,3 @@ internal class StockedCollection<TItem>
         }
     }; 
 }
-
-internal delegate void Iteration<TEntry>(EntryAction<TEntry> entryAction);
-internal delegate void EntryAction<TEntry>(Iterator<(TEntry Item, int Quantity)> current);
-
-internal readonly struct Iterator<TEntry>(List<TEntry> list, int index)
-{
-    private readonly List<TEntry> _list = list;
-    private readonly int _index = index;
-
-    internal TEntry Current
-    {
-        get => _list[_index];
-        set => _list[_index] = value;
-    }
-}
---
+++ Utills/Iteration.cs
@@ -0,0 +1,16 @@
+﻿namespace Cart;
+
+internal delegate void Iteration<TEntry>(EntryAction<TEntry> entryAction);
+internal delegate void EntryAction<TEntry>(Iterator<(TEntry Item, int Quantity)> current);
+
+internal readonly struct Iterator<TEntry>(List<TEntry> list, int index)
+{
+    private readonly List<TEntry> _list = list;
+    private readonly int _index = index;
+
+    internal TEntry Current
+    {
+        get => _list[_index];
+        set => _list[_index] = value;
+    }
+}

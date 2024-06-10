c2f40525c3913e5b8ed60bda373ce6e072106af5
greg-lsk <greg.liaskas@outlook.com>
2024-06-10 08:46:50 +0300

    refactoring using the new fluentAPI

--- Collection/StockedCollection.cs
+++ Collection/StockedCollection.cs
@@ -26,7 +26,7 @@ internal class StockedCollection<TItem>
         get
         {
             int total = 0;
-            Iteration.On(_items, i => {total += i.Current.Quantity;});
+            Iteration.On(_items).Run(i => {total += i.Current.Quantity;});
             return total;
         }
     }
@@ -38,7 +38,7 @@ internal class StockedCollection<TItem>
     }
 
     internal void Delete(TItem item) =>
-    Iteration.On(_items, i =>  
+    Iteration.On(_items).Run(i =>  
     {
         if(_equals(i.Current.Item, item))
         {

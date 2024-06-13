888c08c03910db8bc729479e2a8d9cdafe107061
greg-lsk <greg.liaskas@outlook.com>
2024-06-13 11:51:21 +0300

    draft:: "TryRun()" scaffolding, for stating what to do when the whole collection is iteratted, but the condition was never met

--- Utills/Iteration.cs
+++ Utills/Iteration.cs
@@ -59,8 +59,22 @@ internal readonly struct ConditionalProvider<TEntry>(IEnumerable<TEntry> sequenc
 
     internal TReturn? Run<TReturn>(EntryFunction<TEntry, TReturn> function) 
     => ConditionCore.ActWhen(_sequence, _condition, function);
+
+    internal FinalProvider<TEntry, TReturn> TryRun<TReturn>(EntryFunction<TEntry, TReturn> function) 
+    => new(_sequence, _condition, function);
+    
 }
 
+internal readonly struct FinalProvider<TEntry, TReturn>(IEnumerable<TEntry> sequence,
+                                                        Predicate<TEntry> condition,
+                                                        EntryFunction<TEntry, TReturn> function)
+{
+    private readonly IEnumerable<TEntry> _sequence = sequence;
+    private readonly Predicate<TEntry> _condition = condition;
+    private readonly EntryFunction<TEntry, TReturn> _function = function;
+
+    internal TReturn? Finally(Func<TReturn> action) => default;
+}
 
 internal static class IterationCore
 {    
